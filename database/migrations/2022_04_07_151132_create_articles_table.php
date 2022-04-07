625a16053ff02d123d41e2b43dafeb64eca7dcb7
KOH <gaosheng087@gmail.com>
2022-04-07 15:21:42 +0900

    Articles&Usersテーブル作成#20

---
+++ database/migrations/2022_04_07_151132_create_articles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('base')->nullable();
+            $table->string('taste')->nullable();
+            $table->string('feature')->nullable();
+            $table->string('material', 300)->nullable();
+            $table->string('comment', 500)->nullable();
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
