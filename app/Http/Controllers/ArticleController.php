63f4813807c0b8e939f20e0f0394f052984e0f8c
KOH <gaosheng087@gmail.com>
2022-04-08 07:40:12 +0900

    ArticleContoller編集#28

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,43 +8,11 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        // ダミーデータ
-        $articles = [
-            (object) [
-                'id' => 1,
-                'name' => '名前1',
-                'base' => 'ベース1',
-                'taste' => '味わい1',
-                'feature' => '特徴1',
-                'material' => '材料1',
-                'comment' => 'コメント1',
-                'user_id' => 1,
-                'created_at' => now(),
-            ],
-            (object) [
-                'id' => 2,
-                'name' => '名前2',
-                'base' => 'ベース2',
-                'taste' => '味わい2',
-                'feature' => '特徴2',
-                'material' => '材料2',
-                'comment' => 'コメント2',
-                'user_id' => 1,
-                'created_at' => now(),
-            ],
-            (object) [
-                'id' => 3,
-                'name' => '名前3',
-                'base' => 'ベース3',
-                'taste' => '味わい3',
-                'feature' => '特徴3',
-                'material' => '材料3',
-                'comment' => 'コメント3',
-                'user_id' => 1,
-                'created_at' => now(),
-            ],
-        ];
+        return view('articles.index');
+    }
 
-        return view('articles.index', ['articles' => $articles]);
+    public function create()
+    {
+        return view('articles.create');
     }
 }
