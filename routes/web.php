50257e794b66571e18f6cca1ad6eb6611c17eacd
KOH <gaosheng087@gmail.com>
2022-04-08 07:29:56 +0900

    ルーティング追加#26

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Auth::routes();
 
 Route::get('/', function () {
@@ -17,4 +14,4 @@ Route::get('/search', function () {
 Route::get('/search_result', 'SearchController@index')->name('searches.index');
 Route::get('/search_show/{id}', 'SearchController@show')->name('searches.show');
 
-Route::get('/article', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController');
