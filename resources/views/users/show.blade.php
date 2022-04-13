e8b7543e45625b6ece8bf6eb8c85970ff6023807
KOH <gaosheng087@gmail.com>
2022-04-13 22:27:26 +0900

    showでtabsを使用#80

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,22 +5,11 @@
 @section('content')
     <div class="container" id="user-page">
         @include('users.user')
-        <ul class="nav nav-tabs nav-justified mt-3">
-            <li class="nav-item">
-                <a href="{{ route('users.show', ['name' => $user->name]) }}" class="nav-link text-muted active">
-                    投稿
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('users.likes', ['name' => $user->name]) }}" class="nav-link text-muted">
-                    お気に入り
-                </a>
-            </li>
-        </ul>
+
+        @include('users.tabs', ['hasArticles' => true, 'hasLikes' => false])
+
         @foreach ($articles as $article)
             @include('articles.card')
         @endforeach
-
-
     </div>
 @endsection
