4ca9c6ea2411e53cc16226f2f674b9fea75e0f0c
KOH <gaosheng087@gmail.com>
2022-04-13 22:11:15 +0900

    Blade新規作成と共通化#78

--- resources/views/users/likes.blade.php
+++ resources/views/users/likes.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.article-app')
 
-@section('title', $user->name)
+@section('title', $user->name . '-お気に入り')
 
 @section('content')
     <div class="container" id="user-page">
         @include('users.user')
         <ul class="nav nav-tabs nav-justified mt-3">
             <li class="nav-item">
-                <a href="{{ route('users.show', ['name' => $user->name]) }}" class="nav-link text-muted active">
+                <a href="{{ route('users.show', ['name' => $user->name]) }}" class="nav-link text-muted">
                     投稿
                 </a>
             </li>
             <li class="nav-item">
-                <a href="" class="nav-link text-muted">
+                <a href="{{ route('users.likes', ['name' => $user->name]) }}" class="nav-link text-muted active">
                     お気に入り
                 </a>
             </li>
@@ -20,7 +20,5 @@
         @foreach ($articles as $article)
             @include('articles.card')
         @endforeach
-
-
     </div>
 @endsection
