94783592ae2de1b51f8a92ad757b962ba38bd7db
KOH <gaosheng087@gmail.com>
2022-04-13 16:55:19 +0900

    Vueコンポーネント作成、app.js編集#62

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,11 +9,13 @@
 import './bootstrap'
 import Vue from 'vue'
 import ArticleLike from './components/ArticleLike'
+import FollowButton from './components/FollowButton'
 
 const app = new Vue({
   el: '#app',
   components: {
     ArticleLike,
+    FollowButton,
   }
 })
 
