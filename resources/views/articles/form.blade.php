447f4edebd98290e080f52040bc9c84703ba8082
KOH <gaosheng087@gmail.com>
2022-04-18 20:21:05 +0900

    投稿・編集画面レスポンシブ対応#102

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -10,36 +10,36 @@
                     @if (empty($article->image)) src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
                     @else
                         src="{{ asset('/storage/' . $article->image) }}" @endif
-                    style="max-width:200px; max-height: 265px;">
+                    style="max-width:200px;">
             </p>
             <input type="file" id="image" name="image" accept='image/png,image/jpeg,image/jpg'
                 onchange="previewImage(this);" class="file" value="{{ $article->image ?? old('image') }}">
         </div>
         <div class="form-tp-right">
             <div class="form-group row">
-                <label for="name" class="col-sm-2 col-form-label">名前</label>
-                <div class="col-sm-10">
+                <label for="name" class="col-2 col-form-label">名前</label>
+                <div class="col-10">
                     <input type="text" class="form-control" id="name" name="name" required
                         value="{{ $article->name ?? old('name') }}">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="base" class="col-sm-2 col-form-label">ベース</label>
-                <div class="col-sm-10">
+                <label for="base" class="col-2 col-form-label">ベース</label>
+                <div class="col-10">
                     <input type="text" name="base" class="form-control" id="base" placeholder="ジン、ウォッカ、など"
                         value="{{ $article->base ?? old('base') }}">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="taste" class="col-sm-2 col-form-label">味</label>
-                <div class="col-sm-10">
+                <label for="taste" class="col-2 col-form-label">味</label>
+                <div class="col-10">
                     <input type="text" name="taste" class="form-control" id="taste" placeholder="甘い、辛い、など"
                         value="{{ $article->taste ?? old('taste') }}">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="feature" class="col-sm-2 col-form-label">特徴</label>
-                <div class="col-sm-10">
+                <label for="feature" class="col-2 col-form-label">特徴</label>
+                <div class="col-10">
                     <textarea name="feature" class="form-control" rows="3" id="feature"
                         placeholder="シンプル、さっぱり、など">{{ $article->feature ?? old('feature') }}</textarea>
                 </div>
