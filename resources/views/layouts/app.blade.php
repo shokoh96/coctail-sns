c2fa16f7620eb4d8f127c557f6f9365208e741d9
KOH <gaosheng087@gmail.com>
2022-04-20 22:07:39 +0900

    ナビバー修正#106

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,16 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         ここ酒
                     </a>
+
+                    @auth
+                        <ul class="navbar-nav sp">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('articles.create') }}"><i
+                                        class="fas fa-edit"></i></a>
+                            </li>
+                        </ul>
+                    @endauth
+
                     <button class="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -41,23 +51,35 @@
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item">
                                 <a class="nav-link" href="/search">{{ __('お酒検索') }}</a>
+                                <hr>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('articles.index') }}">{{ __('投稿一覧') }}</a>
+                                <hr>
                             </li>
                         </ul>
 
+                        @auth
+                            <ul class="navbar-nav pc">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('articles.create') }}"><i
+                                            class="fas fa-pen mr-2"></i>{{ __('投稿する') }}</a>
+                                </li>
+                            </ul>
+                        @endauth
 
                         <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto auth">
+                        <ul class="navbar-nav ml-2 auth">
                             <!-- Authentication Links -->
                             @guest
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
+                                    <hr>
                                 </li>
                                 @if (Route::has('register'))
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{ route('register') }}">{{ __('新規登録') }}</a>
+                                        <hr>
                                     </li>
                                 @endif
                             @else
@@ -74,26 +96,7 @@
                                             {{ __('マイページ') }}
                                         </a>
                                         <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            {{ __('ログアウト') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                            class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                                <li class="nav-item dropdown responsive">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        <i class="fas fa-user"></i>{{ Auth::user()->name }}
-                                    </a>
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                                                                                                                                                document.getElementById('logout-form').submit();">
+                                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                             {{ __('ログアウト') }}
                                         </a>
 
