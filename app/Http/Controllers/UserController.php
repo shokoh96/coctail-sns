923ce28d837092e3ae5f99320bf078f15181dd97
KOH <gaosheng087@gmail.com>
2022-04-21 10:13:40 +0900

    フォロー・フォロワーのSQL改善#108

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -35,7 +35,8 @@ class UserController extends Controller
 
     public function followings(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load('followings.followers');
 
         $followings = $user->followings->sortByDesc('created_at');
 
@@ -47,7 +48,8 @@ class UserController extends Controller
 
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load('followers.followers');
 
         $followers = $user->followers->sortByDesc('created_at');
 
