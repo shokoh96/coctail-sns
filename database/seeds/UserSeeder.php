975587ae5ac11f3f8974a35bf9428dd731f0b363
KOH <gaosheng087@gmail.com>
2022-04-25 16:39:34 +0900

    ダミーデータ挿入

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -44,6 +44,36 @@ class UserSeeder extends Seeder
                 'password' => Hash::make('password'),
                 'created_at' => '2022/01/05 00:00:00',
             ],
+            [
+                'name' => 'suzuki',
+                'email' => 'suzuki@test.com',
+                'password' => Hash::make('password'),
+                'created_at' => '2022/01/06 00:00:00',
+            ],
+            [
+                'name' => 'takahashi',
+                'email' => 'takahashi@test.com',
+                'password' => Hash::make('password'),
+                'created_at' => '2022/01/07 00:00:00',
+            ],
+            [
+                'name' => 'watanabe',
+                'email' => 'watanabe@test.com',
+                'password' => Hash::make('password'),
+                'created_at' => '2022/01/08 00:00:00',
+            ],
+            [
+                'name' => 'yamamoto',
+                'email' => 'yamamoto@test.com',
+                'password' => Hash::make('password'),
+                'created_at' => '2022/01/09 00:00:00',
+            ],
+            [
+                'name' => 'nakamura',
+                'email' => 'nakamura@test.com',
+                'password' => Hash::make('password'),
+                'created_at' => '2022/01/10 00:00:00',
+            ],
         ]);
     }
 }
