2b0ff090aabb1668f31391ae32f29b82e3c29007
KOH <gaosheng087@gmail.com>
2022-04-25 10:21:32 +0900

    Article機能SP対応完了#119

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -17,28 +17,28 @@
         </div>
         <div class="form-tp-right">
             <div class="form-group row">
-                <label for="name" class="col-2 col-form-label">名前</label>
+                <label for="name" class="col-2 col-form-label">名前<small class="ml-1">(必須)</small></label>
                 <div class="col-10">
                     <input type="text" class="form-control" id="name" name="name" required
                         value="{{ $article->name ?? old('name') }}">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="base" class="col-2 col-form-label">ベース</label>
+                <label for="base" class="col-2 col-form-label">ベース<small class="ml-1">(任意)</small></label>
                 <div class="col-10">
                     <input type="text" name="base" class="form-control" id="base" placeholder="ジン、ウォッカ、など"
                         value="{{ $article->base ?? old('base') }}">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="taste" class="col-2 col-form-label">味</label>
+                <label for="taste" class="col-2 col-form-label">味<small class="ml-1">(任意)</small></label>
                 <div class="col-10">
                     <input type="text" name="taste" class="form-control" id="taste" placeholder="甘い、辛い、など"
                         value="{{ $article->taste ?? old('taste') }}">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="feature" class="col-2 col-form-label">特徴</label>
+                <label for="feature" class="col-2 col-form-label">特徴<small class="ml-1">(任意)</small></label>
                 <div class="col-10">
                     <textarea name="feature" class="form-control" rows="3" id="feature"
                         placeholder="シンプル、さっぱり、など">{{ $article->feature ?? old('feature') }}</textarea>
@@ -47,14 +47,16 @@
         </div>
     </div>
     <div class="form-group row mt-4">
-        <label for="material" class="col-12 col-form-label">作り方(300文字以内)</label>
+        <label for="material" class="col-12 col-form-label">作り方(300文字以内)<small
+                class="ml-1">(任意)</small></label>
         <div class="col-sm-12">
             <textarea name="material" class="form-control" rows="3" id="material" placeholder=""
                 maxlength="300">{{ $article->material ?? old('material') }}</textarea>
         </div>
     </div>
     <div class="form-group row">
-        <label for="comment" class="col-12 col-form-label">コメント(500文字以内)</label>
+        <label for="comment" class="col-12 col-form-label">コメント(500文字以内)<small
+                class="ml-1">(任意)</small></label>
         <div class="col-sm-12">
             <textarea name="comment" class="form-control" rows="5" id="comment" placeholder=""
                 maxlength="500">{{ $article->comment ?? old('comment') }}</textarea>
